23498d2bd4431d5b938589ae07c56717a266faf0
sneharao <sneharao300@gmail.com>
2024-04-05 15:56:00 +0200

    added mount function , to attach to the custom element in html we want , whether it is container or products project run locally

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,9 +1,21 @@
 import faker from 'faker';
 
-let products = '';
-for(let i = 0; i < 5; i++) {
-  products += `<div>${faker.commerce.productName()}</div>`;
+// mount function to render products, to ensure that, the container doesn't always search for dev-products,
+// while it should mount the products in the div it wants to
+export const mount = (el) => {
+    let products = '';
+    for(let i = 0; i < 5; i++) {
+      products += `<div>${faker.commerce.productName()}</div>`;
+    }
+    
+    console.log(products);
+    el.innerHTML = products;
 }
 
-console.log(products);
-document.querySelector('#dev-products').innerHTML = products;
+if(process.env.NODE_ENV === 'development') {
+    //considering the container will not have a div with id dev-products
+    const el = document.querySelector('#dev-products');
+    if(el) {
+        mount(el);
+    }
+}
