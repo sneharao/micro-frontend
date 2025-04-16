15a5bbb0e6cffd13d993cf12de444e04d159e824
sneharao <sneharao300@gmail.com>
2025-04-16 23:51:22 +0200

    Fixed wrong import

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from '../components/marketingApp';
-import Header from './Header';
+import Header from '../components/Header';
 
 
 export default () => {
